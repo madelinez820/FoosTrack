3a6d16b842a75851d5b2116b9f3ac5d080496151
Madeline Zhang <maddyz@mit.edu>
2019-09-14 15:41:11 -0400

    button toggling

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
-console.log("hi");
+var pointInPlay  = false;
+var pointButton = document.getElementById("pointButton");
+function startPointToggle(){ // TODO: insert the call that triggers video to livestream
+	pointInPlay  = ! pointInPlay;
+	if (pointInPlay) { // TODO: insert the call that triggers video to livestream
+		pointButton.innerHTML = "Click when point is finished!";
+		pointButton.classList.remove('btn-warning');
+		pointButton.classList.add('btn-danger');
+
+	} else{ //TODO: insert the call that triggers video to replay last 5 seconds of point
+		pointButton.innerHTML = "Click to start point!";
+		pointButton.classList.add('btn-warning');
+		pointButton.classList.remove('btn-danger');
+	}
+	console.log(pointInPlay);
+}
