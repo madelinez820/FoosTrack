1eb053cf1a4ad84201246cb08891e9ce086b9353
Madeline Zhang <maddyz@mit.edu>
2019-09-14 17:20:45 -0400

    added video thing

--- js/index.js
+++ js/index.js
@@ -1,16 +1,41 @@
+// hard-coded values for now
+var yellowScore = 9;
+var blackScore = 5;
+var currentVideoPath = "videos/sampleVideo.mp4";
+
+
+
 var pointInPlay  = false;
 var pointButton = document.getElementById("pointButton");
-function startPointToggle(){ // TODO: insert the call that triggers video to livestream
+var currentVideo = document.getElementById("currentVideo");
+
+
+function startPointToggle(){ 
 	pointInPlay  = ! pointInPlay;
 	if (pointInPlay) { // TODO: insert the call that triggers video to livestream
 		pointButton.innerHTML = "Click when point is finished!";
 		pointButton.classList.remove('btn-warning');
 		pointButton.classList.add('btn-danger');
 
+		playLiveStreamVideo();
+
 	} else{ //TODO: insert the call that triggers video to replay last 5 seconds of point
 		pointButton.innerHTML = "Click to start point!";
 		pointButton.classList.add('btn-warning');
 		pointButton.classList.remove('btn-danger');
+
+		playReplayVideo();
 	}
 	console.log(pointInPlay);
+
+	function playLiveStreamVideo(){ // TODO: insert the call that triggers video to livestream
+		currentVideo.src = currentVideoPath;
+		currentVideo.play();
+	}
+
+	function playReplayVideo(){//TODO: insert the call that triggers video to replay last 5 seconds of point
+		currentVideo.src = currentVideoPath;
+		currentVideo.play();
+
+	}
 }
